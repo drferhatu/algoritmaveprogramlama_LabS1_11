e5ed2186f5c61dfd6ed441735bac87f32dc50ca1
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-11-28 00:52:43 +0300

    Create Exercise4_6_Multadd class with methods
    Initial implementation of multadd and expSum methods with placeholders for functionality.

---
+++ Exercise4_6_Multadd.java
@@ -0,0 +1,16 @@
+public class Exercise4_6_Multadd {
+
+    public static double multadd(double a, double b, double c) {
+        // TODO
+        return 0.0;
+    }
+
+    public static double expSum(double x) {
+        // TODO
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        // TODO: test methods
+    }
+}
