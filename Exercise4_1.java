2f67eb4ae4c1cc3ba3ceb5ad9369e0bcad72bac0
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-11-28 00:48:00 +0300

    Add Exercise4_1 class with date printing methods

---
+++ Exercise4_1.java
@@ -0,0 +1,14 @@
+public class Exercise4_1 {
+
+    public static void printAmerican(String day, int date, String month, int year) {
+        // TODO
+    }
+
+    public static void printEuropean(String day, int date, String month, int year) {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        // TODO: test methods
+    }
+}
